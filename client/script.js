a293cfb7443efcfa49c6d985c65fd29441672e7c
Andy Gorbould <gorbould@mail.com>
2022-07-19 10:20:04 +0200

    clientPath is not defined - fix me

--- client/script.js
+++
@@ -1,7 +0,0 @@
-const app = express();
-constclientPath = `${__dirname}/../client`;
-app.use(express.statis(clientPath));
-const server = http.createServer(app);
-server.listen(5500, () => {
-    console.log('server is running on "+port"');
-});
--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
+
+// require express & http
 const express = require('express');
 const http = require('http');
+
+// express & http to host client
+const app = express();
+constclientPath = `${__dirname}/../client`;
+app.use(express.static(clientPath));
+const server = http.createServer(app);
+
+// get server live
+server.listen(5500, () => {
+    console.log('server is running on "+port"');
+});
+
 const io = require('socket.io')(server);
 let socket = io.connect();
 
