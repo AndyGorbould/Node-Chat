d9089ea28e17ad1c77db4e9c69f20f9df62e892a
Andy Gorbould <gorbould@mail.com>
2022-07-19 13:21:29 +0200

    step 11, 12, 13 - possibly in wrong places

--- client/script.js
+++ client/script.js
@@ -4,3 +4,12 @@ let socket = io.connect();    ///// this might be needed here to run
 // checking
 let counter = 0;
 console.log(counter+' someone connected');
+
+
+
+// send message to all (step 12)
+socket.emit('sendToAll', ('message'));
+
+socket.on('sendToAll', (message) =>{
+    io.emit("displayMessage", (message));
+});
